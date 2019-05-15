22e4d19fa9ce10d295d92ee0439d1fb12353527f
chenminken <zbcmb@outlook.com>
2019-05-15 23:53:17 +0800

    change syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#define MAXCALL 20
 
+typedef void(*CALL_PROC)(struct intr_frame*);
 void syscall_init (void);
-
+// void system_write(struct intr_frame*);
 #endif /* userprog/syscall.h */
