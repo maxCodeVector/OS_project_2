e145f5c8f788c2e4c93169698ba824a59618cc73
chenminken <zbcmb@outlook.com>
2019-05-15 23:51:05 +0800

    init call_arr in syscall_init

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,15 +10,16 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  //init call_arr with null
+  for(int i=0;i<MAXCALL;i++){
+    call_arr[i]=NULL;
+  }
+  call_arr[SYS_WRITE] = system_write;
 }
 
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  int No = *(int*)(f->esp);
-  if(call_arr[No] != NULL){
-    call_arr[No](f);
-  }
   printf ("system call!\n");
   thread_exit ();
 }
